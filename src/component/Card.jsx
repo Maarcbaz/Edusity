09a7f10749907c74664489f6a89ad37daa71fb44
Abass Muhammed <olayemiabaz@gmail.com>
2025-01-27 03:12:14 +0100

    Conflict solution (#7)
    * carousel responsive
    
    * carousel functions and responsive
    
    * merge conflict resolved for restructure branch
    
    * created branch from carousel previous commit

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
-
 const Card = ({ card }) => {
 	return (
-		<div className="rounded-xl px-10 py-4 bg-white border border-red-600">
-			<div className="flex gap-3">
-				<div>
-					<img
-						width={'50px'}
-						className="rounded-full border-light_blue border-[3px]"
-						src={card.img}
-						alt={card.alt}
-					/>
-				</div>
-				<div>
-					<h3 className="capitalize text-light_blue  font-bold">{card.name}</h3>
-					<p className="text-sm text-lightText">{card.school}</p>
-				</div>
+		<>
+			<div className="flex items-center  justify-between overflow-hidden gap-1">
+				<section className="my-6">
+					<div className="flex flex-col gap-4 w-[350px] bg-gray-100 shadow-md py-8 px-6 mx-4 rounded-xl bg-primary/10 relative">
+						<div className="flex gap-2">
+							<div>
+								<img
+									width={'50px'}
+									className="rounded-full border-light_blue border-[3px]"
+									src={card.img}
+									alt={card.alt}
+								/>
+							</div>
+							<div>
+								<h3 className="capitalize text-light_blue  font-bold">
+									{card.name}
+								</h3>
+								<p className="text-sm text-lightText">{card.school}</p>
+							</div>
+						</div>
+						<div className="text-lightText text-sm mt-1">
+							<p>{card.speech}</p>
+						</div>
+					</div>
+				</section>
 			</div>
-			<div className="text-lightText text-sm mt-3">
-				<p>{card.speech}</p>
-			</div>
-		</div>
+		</>
 	);
 };
 
